f41a05a4ba2c1f00f5d8f642089db90285ff61ad
James Kim <jamesjunsungkim@gmail.com>
2023-02-04 17:38:28 -0800

    resolve spm error

--- Package.swift
+++ Package.swift
@@ -20,14 +20,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "ShopliveSDKCommon",
-                      path: "ShopliveSDKCommon.xcframework"
-                     ),
+                      path: "ShopliveSDKCommon.xcframework"),
         .target(name: "ShopliveSDKCommonTargets",
                 dependencies: [
-                        .target(name: "ShopliveSDKCommon"),
-                        .product(name: "Alamofire", package: "Alamofire"),
-                    ],
-                path: "ShopliveSDKCommonTargets"
-               ),
+                    .target(name: "ShopliveSDKCommon"),
+                    .product(name: "Alamofire", package: "Alamofire"),
+                ],
+                path: "Sources")
     ]
 )
---
+++ Sources/ShopliveSDKCommon.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by James Kim on 2/4/23.
+//
+
+import Foundation
